54f0b2011017e655983099902c363a95436946c5
Engulfedleader2 <engulfedleader@gmail.com>
2025-09-01 19:32:20 +0200

    Initial Commit

---
+++ Screenshot_Analyzer/Screenshot_AnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screenshot_AnalyzerApp.swift
+//  Screenshot_Analyzer
+//
+//  Created by Israel on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Screenshot_AnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
